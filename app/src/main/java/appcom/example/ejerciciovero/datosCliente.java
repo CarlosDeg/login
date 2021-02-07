b533aea6a0490c53d0cec55efb456a7b7dbad493
Carlos Deg <degaonavcarlos@gmail.com>
2021-02-07 01:21:15 -0600

    ed

---
+++ app/src/main/java/appcom/example/ejerciciovero/datosCliente.java
@@ -0,0 +1,13 @@
+package appcom.example.ejerciciovero;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class datosCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_cliente);
+    }
+}
